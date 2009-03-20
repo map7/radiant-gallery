9c0af4c6ab1adeb96091fb59f660d40106046ed2
Tom Cowell <tc@eightsquarestudio.com>
2009-03-20 20:51:48 +0000

    tidy up tags to get keywords working across the board

--- lib/gallery_item_tags.rb
+++ lib/gallery_item_tags.rb
@@ -119,7 +119,7 @@ module GalleryItemTags
   tag "gallery:item:keywords" do |tag|      
     item = find_item(tag)    
     joiner = tag.attr['separator'] ? tag.attr['separator'] : ' '
-    item.keywords.gsub!(/\,/, joiner);
+    item.keywords.gsub(/\,/, joiner);
   end 
   
   desc %{
